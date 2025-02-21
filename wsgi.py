5375daa445d95fe31faf61e9587f5516f84a084d
veetir <veeti.reis2@gmail.com>
2025-02-21 12:52:10 +0200

    .

--- wsgi.py
+++ wsgi.py
@@ -1,24 +1,24 @@
-
 import os, sys
 
 # Add the pheweb package into the PYTHONPATH so that we can import it.
 # This assumes that you cloned pheweb from github.  If you installed with pip, maybe this has no effect?
-sys.path.insert(0, '/home/veetir/Documents/pheweb')
+sys.path.insert(0, "/home/veetir/Documents/pheweb")
 
 # Activate a virtual environment to get pheweb's dependencies.
-path = os.path.join('/home/veetir/Documents/pheweb/venv/bin/activate_this.py')
+path = os.path.join("/home/veetir/Documents/pheweb/venv/bin/activate_this.py")
 with open(path) as f:
-    code = compile(f.read(), path, 'exec')
+    code = compile(f.read(), path, "exec")
     exec(code, dict(__file__=path))
 
 # `data_dir` is the directory that contains `config.py` and `generated-by-pheweb/`.
 data_dir = os.path.dirname(os.path.abspath(__file__))
-os.environ['PHEWEB_DATADIR'] = data_dir
+os.environ["PHEWEB_DATADIR"] = data_dir
 
 # Load `config.py`.
-config_filepath = os.path.join(data_dir, 'config.py')
+config_filepath = os.path.join(data_dir, "config.py")
 assert os.path.exists(config_filepath)
 import pheweb.conf
+
 pheweb.conf.load_overrides_from_file(config_filepath)
 
 # WSGI uses the variable named `application`.
