0c358cbeae7541761f965d9a65338ca059219af1
veetir <veeti.reis2@gmail.com>
2025-04-10 15:08:56 +0300

    cleanup

--- pheweb/serve/static/gene.js
+++ pheweb/serve/static/gene.js
@@ -1,57 +1,58 @@
 'use strict';
 
-
 function populate_streamtable(data) {
-    $(function () {
-      var table = $('#stream_table').DataTable({
-        data: data,
-        dom: 'lrtip',
-        paging: true,
-        searching: true,
-        info: false,
-        ordering: true,
-        columns: [
-          {
-            title: "Top p-value in gene",
-            data: "assoc.pval",
-            render: function (pval, type, row, meta) {
-              return (pval === 0)
-                ? '≤1e-320'
-                : parseFloat(pval).toExponential(1);
-            }
-          },
-          {
-            title: "Phenotype",
-            data: null,
-            render: function (data, type, row, meta) {
-              var phenoData = data.pheno;
-              var label = phenoData.phenostring || phenoData.phenocode;
-              if (phenoData.phenocode === window.pheno.phenocode) {
-                return label;
-              } else {
-                var baseUrl = "{{ url_for('.region_page', phenocode='', region='').rstrip('/') }}";
-                var url = baseUrl + "/" + phenoData.phenocode + "/gene/" + window.gene_symbol;
-                return '<a style="color:black" href="' + url + '">' + label + '</a>';
-              }
+  $(document).ready(function () {
+    var table = $('#stream_table').DataTable({
+      data: data,
+      dom: 'lrtip',  // Custom layout: no default search box.
+      paging: true,
+      searching: true,
+      info: false,
+      ordering: true,
+      columns: [
+        {
+          title: "Top p-value in gene",
+          data: "assoc.pval",
+          render: function (pval, type, row, meta) {
+            return (pval === 0)
+              ? '≤1e-320'
+              : parseFloat(pval).toExponential(1);
+          }
+        },
+        {
+          title: "Phenotype",
+          data: null,
+          render: function (data, type, row, meta) {
+            var phenoData = data.pheno;
+            var label = phenoData.phenostring || phenoData.phenocode;
+            if (phenoData.phenocode === window.pheno.phenocode) {
+              return label;
+            } else {
+              var baseUrl = "{{ url_for('.region_page', phenocode='', region='').rstrip('/') }}";
+              var url = baseUrl + "/" + phenoData.phenocode + "/gene/" + window.gene_symbol;
+              return '<a style="color:black" href="' + url + '">' + label + '</a>';
             }
           }
-        ]
-      });
-
-      table.rows().every(function (rowIdx, tableLoop, rowLoop) {
-        var rowData = this.data();
-        if (rowData.pheno.phenocode === window.pheno.phenocode) {
-          $(this.node()).css("background-color", "lightblue");
-        } else {
-          $(this.node())
-            .css("cursor", "pointer")
-            .on("click", function () {
-              var baseUrl = "{{ url_for('.region_page', phenocode='', region='').rstrip('/') }}";
-              var url = baseUrl + "/" + rowData.pheno.phenocode + "/gene/" + window.gene_symbol;
-              window.location = url;
-            });
         }
-      });
+      ]
     });
+
+    // For each row, set background or attach onclick handler based on phenotype match.
+    table.rows().every(function () {
+      var rowData = this.data();
+      if (rowData.pheno.phenocode === window.pheno.phenocode) {
+        $(this.node()).css("background-color", "lightblue");
+      } else {
+        $(this.node())
+          .css("cursor", "pointer")
+          .on("click", function () {
+            var baseUrl = "{{ url_for('.region_page', phenocode='', region='').rstrip('/') }}";
+            var url = baseUrl + "/" + rowData.pheno.phenocode + "/gene/" + window.gene_symbol;
+            window.location = url;
+          });
+      }
+    });
+  });
 }
+
 populate_streamtable(window.significant_phenos);
