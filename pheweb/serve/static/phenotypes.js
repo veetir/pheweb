60bb30cdbe70deaafacd58bd8f12a5463677128e
veetir <veeti.reis2@gmail.com>
2025-04-10 14:34:44 +0300

    cleanup: old logic

--- pheweb/serve/static/phenotypes.js
+++ pheweb/serve/static/phenotypes.js
@@ -1,62 +1,86 @@
 'use strict';
 
 function populate_streamtable(phenotypes) {
-    $(function() {
-        // This is mostly copied from <https://michigangenomics.org/health_data.html>.
-        var data = phenotypes;
-        // data = _.sortBy(data, _.property('pval'));
-        var template = _.template($('#streamtable-template').html());
-        var view = function(pheno) {
-            return template({h: pheno});
-        };
-        var $found = $('#streamtable-found');
-        var count = data.length;
-        $found.text(count + (count === 1 ? " phenotype" : " phenotypes"));
-
-        var callbacks = {
-            pagination: function(summary){
-                var text;
-                if ($.trim($('#search').val()).length > 0){
-                    text = summary.total + (summary.total === 1 ? " matching phenotype" : " matching phenotypes");
-                } else {
-                    text = count + (count === 1 ? " phenotype" : " phenotypes");
-                }
-                $found.text(text);
-                patchPaginationMarkup();
+  $(document).ready(function() {
+    var data = phenotypes;
+    var table = $('#stream_table').DataTable({
+      data: data,
+      dom: 'lrtip',
+      columns: [
+        {
+          data: 'category',
+          title: 'Category'
+        },
+        {
+          data: null,
+          title: 'Phenotype',
+          render: function(data, type, row, meta) {
+            var label = data.phenostring || data.phenocode;
+            return '<a style="color:black" href="' + window.model.urlprefix + '/pheno/' + data.phenocode + '">' +
+                     label +
+                   '</a>';
+          }
+        },
+        {
+          data: 'gc_lambda_hundred',
+          title: 'GCλ0.01',
+          render: function(data, type, row, meta) {
+            return (data !== null) ? parseFloat(data).toFixed(2) : '';
+          }
+        },
+        {
+          data: 'num_peaks',
+          title: '#Loci<5e-8'
+        },
+        {
+          data: null,
+          title: 'Top variant in pheno',
+          render: function(data, type, row, meta) {
+            var variantText = data.chrom + ':' + data.pos.toLocaleString() + ' ' +
+                              data.ref + ' / ' + data.alt;
+            if (data.rsids) {
+              variantText += ' (' + data.rsids.split(',').join(', ') + ')';
             }
+            return '<a style="color:black" href="' + window.model.urlprefix + '/variant/' +
+                   data.chrom + '-' + data.pos + '-' + data.ref + '-' + data.alt + '">' +
+                   variantText +
+                   '</a>';
+          }
+        },
+        {
+          data: 'pval',
+          title: 'P-value',
+          render: function(data, type, row, meta) {
+            return (data === 0) ? '≤1e-320' : parseFloat(data).toExponential(1);
+          }
+        },
+        {
+          data: 'nearest_genes',
+          title: 'Nearest Gene(s)',
+          render: function(data, type, row, meta) {
+            var genes = data.split(",");
+            return genes.map(function(gene) {
+              return '<a style="color:black" href="' + window.model.urlprefix +
+                     '/gene/' + gene + '?include=' + row.chrom + '-' + row.pos + '">' +
+                     '<i>' + gene + '</i></a>';
+            }).join(', ');
+          }
         }
-
-        var options = {
-            view: view,
-            search_box: '#search',
-            callbacks: callbacks,
-            pagination: {
-                span: 5,
-                next_text: 'Next <i class="bi bi-arrow-right"></i>',
-                prev_text: '<i class="bi bi-arrow-left"></i> Previous',
-                per_page_select: false,
-                per_page_opts: [100]
-            }
-        }
-
-        function patchPaginationMarkup() {
-            $('.st_pagination').find('ul.pagination').each(function() {
-                $(this).find('li').each(function() {
-                    $(this).addClass('page-item');
-                    const $a = $(this).find('a');
-                    if ($a.length) {
-                        $a.addClass('page-link');
-                        if ($a.hasClass('active')) {
-                            $a.removeClass('active');
-                            $(this).addClass('active');
-                        }
-                    }
-                });
-            });
-        }
-        
-        $('#stream_table').stream_table(options, data);
-        patchPaginationMarkup();
+      ],
+      paging: true,
+      searching: true,
+      info: true,
+      drawCallback: function(settings) {
+        var api = this.api();
+        var totalRows = api.rows().count();
+        var filteredRows = api.rows({ filter: 'applied' }).count();
+        var count = $.trim($('#search').val()) !== "" ? filteredRows : totalRows;
+        var text = count + " " + (count === 1 ? "phenotype" : "phenotypes");
+        $('#streamtable-found').text(text);
+      }
     });
+    $('#search').on('keyup', function() {
+      table.search(this.value).draw();
+    });
+  });
 }
-
